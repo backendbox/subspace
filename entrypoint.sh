694e10a6b3bd4a5fed36f747217e5fe881d2b46c
backendbox <contact@backendbox.net>
2019-10-22 05:51:17 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -123,37 +123,6 @@ ip addr add fd00::10:97:1/112 dev wg0
 wg setconf wg0 /data/wireguard/server.conf
 ip link set wg0 up
 
-# dnsmasq service
-if ! test -d /etc/sv/dnsmasq ; then
-    cat <<DNSMASQ >/etc/dnsmasq.conf
-    # Only listen on necessary addresses.
-    listen-address=127.0.0.1,10.99.97.1,fd00::10:97:1
-
-    # Never forward plain names (without a dot or domain part)
-    domain-needed
-
-    # Never forward addresses in the non-routed address spaces.
-    bogus-priv
-DNSMASQ
-
-    mkdir /etc/sv/dnsmasq
-    cat <<RUNIT >/etc/sv/dnsmasq/run
-#!/bin/sh
-exec /usr/sbin/dnsmasq --no-daemon
-RUNIT
-    chmod +x /etc/sv/dnsmasq/run
-
-# dnsmasq service log
-    mkdir /etc/sv/dnsmasq/log
-    mkdir /etc/sv/dnsmasq/log/main
-    cat <<RUNIT >/etc/sv/dnsmasq/log/run
-#!/bin/sh
-exec svlogd -tt ./main
-RUNIT
-    chmod +x /etc/sv/dnsmasq/log/run
-    ln -s /etc/sv/dnsmasq /etc/service/dnsmasq
-fi
-
 # subspace service
 if ! test -d /etc/sv/subspace ; then
     mkdir /etc/sv/subspace
